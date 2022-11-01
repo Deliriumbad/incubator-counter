6e43412138ca0eb26e08ae673a60a0243b1b9dfb
Vit <vit_waladzko@mail.ru>
2022-11-02 02:46:11 +0300

    Refactor with Redux (refactoring)

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import './App.css';
 import {Counter} from "./components/counter/Counter";
 import style from './app.module.css';
 import {Button} from "./components/button/Button";
